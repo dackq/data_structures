a76c68d0114983a4ccd621188bf5013e1a9909cd
Dakota Reed <dakky@Dakotas-MBP>
2022-03-25 14:51:44 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include "linked_list.h"
 
-int main () {
-	LinkedList *list = create_linked_list ();
-	push (list, 1);
-	push (list, 2);
-
+void print_list (LinkedList *list) {
 	Node* current = list->head;
+	int count = 0;
 
 	while (current != NULL) {
-		printf("%d\n", current->value);
+		printf("%d: %d\n", count, current->value);
+		count++;
 		current = current->next;	
 	}
+
+	printf("\n");
 }
+
+int main () {
+	LinkedList *list = create_linked_list ();
+	add (list, 1);
+	add (list, 2);
+	
+	print_list (list);
+
+	add_at_index (list, 3, 1);
+
+	print_list (list);
+
+	push (list, 4);
+
+	print_list (list);
+}
+
